d73476a25f2b6929d31444cf6a2f3c9279fea9c9
Danny Mayer <mayermediaco@gmail.com>
2019-04-07 17:36:29 -0500

    update Map component

--- src/components/mapContainer.js
+++ src/components/mapContainer.js
@@ -5,7 +5,7 @@ const MapContainer = () => {
   return (
     <section className="section">
       <div className="container">
-        <div className="column is-three-fifths is-offset-one-fifth">
+        <div className="column is-three-fifths is-two-fifth-mobile is-offset-one-fifth">
           <div className="map-section">
             <div className="title is-size-3 has-text-centered">
               <h2>Find me here</h2>
@@ -14,9 +14,7 @@ const MapContainer = () => {
               <Map />
             </section>
             <div>
-              <p className="subtitle has-text-centered">
-                Address: 1200 South York Road Suite 4240 Elmhurst, IL 60126
-              </p>
+              <p className="subtitle has-text-centered">Address: 1200 South York Road Suite 4240 Elmhurst, IL 60126</p>
             </div>
           </div>
         </div>
